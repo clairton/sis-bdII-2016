3ce2a2d6b42b6678a040a190f4355be7ca3ccd6d
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2016-07-29 22:32:35 -0300

    Aula 29-06

--- agregação.sql
+++ agregação.sql
@@ -1,34 +1,100 @@
---Contando itens
-SELECT COUNT(*) FROM items;
-
---Contanto com Where
-SELECT COUNT(*) FROM items WHERE product_id = 0;
-
---Junção com products;
-SELECT COUNT(*) FROM items
-INNER JOIN products ON products.id = items.product_id
-WHERE name='Iron Iron';
-	
---Agrupando
+﻿--Contando itens
 SELECT 
-	products.id, name, COUNT(products.id)
-FROM items
-INNER JOIN products ON products.id = items.product_id
-WHERE name LIKE '%Ir%'
-GROUP BY products.id
-ORDER BY name
-	
+	count(*) 
+FROM 
+	products;
+
+--Contando items que nao tem vendas
+SELECT 
+	count(*) 
+FROM 
+	products
+WHERE
+	products.id NOT IN(
+		SELECT DISTINCT(product_id) FROM items
+	);
+
+--Exemplo de junção no where
 SELECT
-	products.id, products.name, SUM(items.quantity) AS total "
-FROM items
-INNER JOIN products ON items.product_id = products.id
-WHERE products.name = 'Ice Tea Iron'
-GROUP BY products.id
-	
+	products.id, products.name
+FROM
+	products, items
+WHERE
+	products.id = items.product_id
+ORDER BY
+	products.id
+--exemplo de junção com join
+SELECT
+	products.id, products.name
+FROM
+	products
+	INNER JOIN items ON products.id = items.product_id
+ORDER BY
+	products.id
+
+
+--Selecionar Quantos Nome do Cliente e quantas compras ele efetuou
+SELECT
+	first_name, last_name, count(invoices.customer_id) as count
+FROM
+	customers
+	INNER JOIN invoices ON invoices.customer_id = customers.id
+GROUP BY
+	invoices.customer_id, customers.first_name, last_name
+ORDER BY  customers.first_name
+--Selecionar quantos pedidos foram feitos por rua
+SELECT
+	customers.street, count(invoices.customer_id) as count
+FROM
+	customers
+	INNER JOIN invoices ON invoices.customer_id = customers.id
+GROUP BY
+	invoices.customer_id, customers.street
+ORDER BY  customers.street
+
+--Media do valor dos produtos
+SELECT AVG(price) FROM products;
 	
 --Fazer a media de preço de venda dos produtos iniciados com 'Chair'
-SELECT products.id, products.name, AVG(items.cost) AS nao_quero_sabe
-FROM items
-INNER JOIN products ON products.id = items.product_id
-WHERE products.name LIKE 'chair%'
-GROUP BY products.id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--Contanto com Where
+
+--Junção com products;
+	
+--Agrupando
