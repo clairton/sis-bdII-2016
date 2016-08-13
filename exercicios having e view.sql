4e8a4c9dccb5201c7689c6c945e7b16ffe76ab4b
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2016-08-12 22:21:57 -0300

    Exercicios de fixação

---
+++ exercicios having e view.sql
@@ -0,0 +1,4 @@
+/*5- Mostrar os clientes que tem o mesmo nome(first_name) e quantas vezes o nome se repete.*/
+
+
+/*4- Mostrar os produtos vendidos na rua chamada 'Olten' com o quantidade abaixo da quantidade média.*/
